e36aac7234b32db1ac863895043b9773b48f791b
Luke <l.duboispagnon@gmail.com>
2022-01-28 08:58:40 +0100

    add tests

--- tests/cart.test.js
+++ tests/cart.test.js
@@ -3,6 +3,7 @@ const timeout = 15000;
 // Test des fonctionnalités de gestion du panier
 describe("Cart features", () => {
     let page;
+    let count;
 
     test('add to cart', async () => {
         await page.goto(process.env.TESTED_WEBSITE);
@@ -14,6 +15,12 @@ describe("Cart features", () => {
         await page.waitForSelector('btn btn_primary btn_small btn_inventory');
 
     }, timeout);
+
+    test('remove cart', async () => {
+        await page.click('#remove-sauce-labs-backpack')
+        count = await page.$eval('.shopping_cart_badge', e => e.innerHTML)
+        expect(parseInt(count)).toBe(5)
+    });
 
 
     // cette fonction est lancée avant chaque test de cette
--- tests/checkout.test.js
+++ tests/checkout.test.js
@@ -5,21 +5,37 @@ describe("Checkout process", () => {
     let page;
 
     test('checkout', async () => {
-        await page.goto(process.env.TESTED_WEBSITE);
-        await page.waitForSelector('my_login_selector');
-        await page.type('my_login_selector', process.env.TEST_LOGIN);
-        await page.type('my_password_selector', process.env.TEST_PASSWORD);
-
-        // à compléter
+        await page.waitForSelector('.shopping_cart_link')
+        await page.click('.shopping_cart_link')
+        expect(page.url()).toBe('https://www.saucedemo.com/cart.html')
 
     }, timeout);
 
+    test('checkout  complete', async () => {
+        await page.waitForSelector('#finish')
+        await page.click('#finish')
+        expect(page.url()).toBe('https://www.saucedemo.com/checkout-complete.html')
+    }, timeout);
+
+    test('info form', async () => {
+        await page.waitForSelector('#checkout')
+        await page.click('#checkout')
+        expect(page.url()).toBe('https://www.saucedemo.com/checkout-step-one.html')
+    }, timeout);
 
     // cette fonction est lancée avant chaque test de cette
     // série de tests
     beforeAll(async () => {
         // ouvrir un onglet dans le navigateur
         page = await global.__BROWSER__.newPage()
+
+        await page.goto(process.env.TESTED_WEBSITE);
+        await page.waitForSelector('#user-name');
+        await page.waitForSelector('#login-button')
+        await page.type('#user-name', process.env.TEST_LOGIN_SUCCESS);
+        await page.type('#password', process.env.TEST_PASSWORD);
+        await page.click('#login-button')
+        expect(page.url()).toBe('https://www.saucedemo.com/inventory.html')
     }, timeout)
 
 });
