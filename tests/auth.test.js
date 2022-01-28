6b48e0e8d1f0fca7397f704409aff739708054f0
Luke <l.duboispagnon@gmail.com>
2022-01-28 11:25:31 +0100

    chore tests + deploy

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -5,7 +5,6 @@ describe("Auth features", () => {
     let page;
 
     test('login', async () => {
-        await page.goto(process.env.TESTED_WEBSITE);
         await page.waitForSelector('#login_button_container');
         await page.type('#user-name', process.env.TEST_LOGIN);
         await page.type('#password', process.env.TEST_PASSWORD);
@@ -13,25 +12,15 @@ describe("Auth features", () => {
         expect(page.url()).toBe("https://www.saucedemo.com/inventory.html");
     }, timeout);
 
-    test('logout', async () =>{
-        await page.goto(process.env.TESTED_WEBSITE);
-        await page.waitForSelector('#login_button_container');
-        await page.type('#user-name', process.env.TEST_LOGIN);
-        await page.type('#password', process.env.TEST_PASSWORD);
-        await page.click('#login-button');
-        await page.click('.bm-burger-button');
-        await page.waitForSelector('#bm-item-list');
-        await page.waitFor(500);
-        await page.click('#bm-item menu-item');
-        expect(page.url()).toBe("https://www.saucedemo.com");
-
-    }, timeout);
-
-    // cette fonction est lancée avant chaque test de cette
-    // série de tests
     beforeAll(async () => {
         // ouvrir un onglet dans le navigateur
         page = await global.__BROWSER__.newPage()
     }, timeout)
+
+    beforeEach(async () => {
+        await page.goto(process.env.TESTED_WEBSITE);
+        await page.waitForSelector('#user-name');
+        await page.waitForSelector('#login-button')
+    });
 
 });
--- tests/cart.test.js
+++ tests/cart.test.js
@@ -2,32 +2,34 @@ const timeout = 15000;
 
 // Test des fonctionnalités de gestion du panier
 describe("Cart features", () => {
-    let page;
-    let count;
+    let page
+    let count
 
-    test('add to cart', async () => {
-        await page.goto(process.env.TESTED_WEBSITE);
-        await page.waitForSelector('#login_button_container');
-        await page.type('#user-name', process.env.TEST_LOGIN);
-        await page.type('#password', process.env.TEST_PASSWORD);
-        await page.click('#login-button');
-        expect(page.url()).toBe("https://www.saucedemo.com/inventory.html");
-        await page.waitForSelector('btn btn_primary btn_small btn_inventory');
+    test('add items', async () => {
+        await page.waitForSelector('.btn_inventory')
+        const buttons = await page.$$('.btn_inventory')
 
+        for (const btn of buttons) {
+            await btn.click()
+            await page.waitForSelector('.shopping_cart_badge')
+        }
+        expect(parseInt(count)).toBe(6)
     }, timeout);
 
     test('remove cart', async () => {
         await page.click('#remove-sauce-labs-backpack')
         count = await page.$eval('.shopping_cart_badge', e => e.innerHTML)
         expect(parseInt(count)).toBe(5)
-    });
+    }, timeout);
 
-
-    // cette fonction est lancée avant chaque test de cette
-    // série de tests
     beforeAll(async () => {
         // ouvrir un onglet dans le navigateur
         page = await global.__BROWSER__.newPage()
-    }, timeout)
-
+        await page.goto(process.env.TESTED_WEBSITE);
+        await page.waitForSelector('#user-name');
+        await page.waitForSelector('#login-button')
+        await page.type('#user-name', process.env.TEST_LOGIN_SUCCESS);
+        await page.type('#password', process.env.TEST_PASSWORD);
+        await page.click('#login-button')
+    }, timeout);
 });
